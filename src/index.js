7360bf48ace681f0ae50be3d10297fef34445db5
kirstydevlin1 <kirsty.leanne.devlin@gmail.com>
2019-10-27 02:19:31 +0000

    added clocwork request, to send a reply back to the user

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import chalk   from 'chalk'
 import path    from 'path'
 
 import config  from './config'
+var clockwork = require('../node_modules/clockwork')({key:'API_KEY_GOES_HERE'})
 
 const fs = require('fs');
 
@@ -13,6 +14,15 @@ app.use('/', express.static(path.resolve(__dirname, '/../public')))
 app.get('/recordnumber', (req, res) => {
   fs.appendFileSync('numbers.txt', req.query.from + '\n');
   console.log(req.query.from)
+
+  clockwork.sendSms({To: '447539968069', Content: 'Learn your phone number!'},
+    function (error, resp) {
+      if (error) {
+        console.log('Something went wrong', error);
+      } else {
+        console.log('Message sent', resp.responses[0].id);
+      }
+    });
   res.send({
     message: 'Join the Sunset Club today!'
   })
