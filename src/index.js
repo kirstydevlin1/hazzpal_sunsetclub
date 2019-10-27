ae0e2f0530264f2a60e447ccf54f528fac29e5a6
kirstydevlin1 <kirsty.leanne.devlin@gmail.com>
2019-10-27 13:38:10 +0000

    temp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import chalk   from 'chalk'
 import path    from 'path'
-
+import https from 'https'
 import config  from './config'
-var clockwork = require('../node_modules/clockwork')({key:'API_KEY_GOES_HERE'})
-
-const fs = require('fs');
-
+var endData
+var clockwork = require('../node_modules/clockwork')({key:'47cd1164a537ef92741bec883609173f51af17f3'})
+var numbers = require('./numbers.txt')
+const fs = require('fs')
 const app = express()
+const router = express.Router();
 
 app.use('/', express.static(path.resolve(__dirname, '/../public')))
 
-app.get('/recordnumber', (req, res) => {
+router.get('/recordnumber', (req, res) => {
   fs.appendFileSync('numbers.txt', req.query.from + '\n');
   console.log(req.query.from)
 
@@ -42,4 +43,42 @@ app.listen(config.PORT, () => {
   log('\n')
 })
 
-export default app
+//Sends everyone (on the numbers.json file) the sunset and sunrise times
+app.get('/sunset', (req, res) => {
+  //Grabs sunrise and sunset data from an API
+  https.get('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today',(resp)=>{
+    let data = '';
+    resp.on('data',(chunk)=>{
+      data += chunk;
+    });
+    resp.on('end', () => {
+      //Translate it to a javascript object
+      endData = JSON.parse(data);
+      console.log('endData:'+endData);
+      var listofNos = numbers;
+      //Clockwork function at the bottom of this script
+      clockworkAPI(endData,listofNos);
+      res.send({
+        message: 'message sent'
+      });
+    });
+  })
+})
+
+function clockworkAPI(endData, listofNos) {
+  var fullListOfNos = [];
+  for(var i=0;i<listofNos.length;i++){
+    fullListOfNos[i] = { To: listofNos[i], Content: 'Sunset is at '+endData.results.sunset+' and Sunrise is at '+endData.results.sunrise }
+  }
+  console.log(fullListOfNos);
+  clockwork.sendSms([fullListOfNos], 
+  function(error, resp) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Message sent',resp.responses[0].id,endData.results.sunset,endData.results.sunrise,listofNos);
+    }
+});
+}
+
+export default app;
